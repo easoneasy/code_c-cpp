6ee8ea4282b761c47eeca0d93dfe7b929bb2e27f
xeasonzq <1696220214@qq.com>
2026-06-30 21:48:24 +0800

    leetcode day1

---
+++ 04_LeetCode/day630/01_136.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+/*
+ * 只出现一次的数字
+ * 给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。
+ * 找出那个只出现了一次的元素。
+ */
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(auto num : nums)
+        {
+            ans ^= num;
+        }
+        return ans;
+    }
+};
+
+
+int main(int argc,char *argv[])
+{
+    vector<int> nums = {4,1,2,1,2};
+    Solution s;
+    cout << s.singleNumber(nums) << endl;
+
+    return 0;
+}
